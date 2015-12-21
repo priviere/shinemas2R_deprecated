5f7bd3e2cdbe4c90b969d4db363a4639a4ae5d16
priviere <pierre@semencespaysannes.org>
2015-12-21 13:47:14 +0100

    update messages

--- R/shinemas2R-package.r
+++ R/shinemas2R-package.r
@@ -4,5 +4,4 @@
 #' @docType package
 #' @description
 #' The R package \code{shinemas2R} analyses outputs from the data base SHiNeMaS (Seeds History and Network Management System). It permits desciptive analysis as well as formatting data for existing R packages to perform statistical analysis.
-#' further details in the description files and on the web site : A.FAIRE.org
 NULL
