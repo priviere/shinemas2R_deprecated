10143f91fba8f573a23b5eac9e3795c0e2b5644f
priviere <pierre@semencespaysannes.org>
2016-02-04 15:41:50 +0100

    update edge and vertex

--- R/is.get.data.output.R
+++ R/is.get.data.output.R
@@ -34,7 +34,60 @@ is.get.data.output = function(
 	vec_shinemas2R.object = c("network", "data-classic-seed-lots", "data-classic-relation", "data-S-seed-lots", "data-S-relation", "data-SR-seed-lots", "data-SR-relation")
 	if(!is.element(shinemas2R.object, vec_shinemas2R.object)) { stop("shinemas2R.object must be: ", paste(vec_shinemas2R.object, collapse = ", ")) }
 
+	# 2. shinemas2R.object == "network"
+	if( shinemas2R.object == "network") { 
+		
+		mess = "With shinemas2R.object == \"network\", data must be a list with the following three elements: \"network\", \"network.info\", \"Mdist\"." 
+		if( !is.list(data) ) { stop(mess) }
+		if( names(data)[1] != "network" ) { stop(mess) }
+		if( names(data)[2] != "network.info" ) { stop(mess) }
+		if( names(data)[3] != "Mdist" ) { stop(mess) }
+		
+		# 2.1. "network"
+		if( !is.null(data[[1]]) ) {
+			if( !is.network(data[[1]]) ) { stop("The first element of data must be NULL or a network object.") }
+		}
+		
+		update.vertex.attributes = function(n = data, v.att = "year"){
+			test = get.vertex.attribute(n, v.att)
+			if( length(is.na(test)) == length(test) ) { 
+				set.vertex.attribute(n, v.att, value = NULL)	
+				message("vertex attributes \",", v.att,",\" has been set to NULL.")
+			}			
+		}
+		update.vertex.attributes(n = data[[1]], v.att = "year")
+		update.vertex.attributes(n = data[[1]], v.att = "person")
+		update.vertex.attributes(n = data[[1]], v.att = "germplasm")
+		update.vertex.attributes(n = data[[1]], v.att = "germplasm.type")
+		update.vertex.attributes(n = data[[1]], v.att = "sex")
+		
+		update.edge.attributes = function(n = data, e.att = "relation"){
+			test = get.edge.attribute(n, v.att)
+			if( length(is.na(test)) == length(test) ) { 
+				set.edge.value(n, e.att, value = NULL)	
+				message("edge attributes \",", e.att,",\" has been set to NULL.")
+			}			
+		}
+		update.edge.attributes(n = data[[1]], e.att = "relation")
+		update.edge.attributes(n = data[[1]], e.att = "generation")
+		
+		# 2.2. "network.info"
+		# 2.3. "Mdist"
+		
+		}
+
+	if( shinemas2R.object == "data-classic-seed-lots") { }
+
+	if( shinemas2R.object == "data-classic-relation") { }
+
+	if( shinemas2R.object == "data-S-seed-lots") { }
+
+	if( shinemas2R.object == "data-S-relation") { }
+
+	if( shinemas2R.object == "data-SR-seed-lots") { }
 	
+	if( shinemas2R.object == "data-SR-relation") { }
+		
 	}
 
 
