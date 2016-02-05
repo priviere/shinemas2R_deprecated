3fca6e400611512c5bf75d0f00daad3cefd1e779
priviere <pierre@semencespaysannes.org>
2016-02-05 09:25:56 +0100

    update bugs for network

--- R/is.get.data.output.R
+++ R/is.get.data.output.R
@@ -25,10 +25,10 @@
 #' 	 \itemize{
 #' 	 	\item "network" must be a network object. It can be NULL. Vertex attributes can be put on "year", "person", "germplasm", germplasm.type" and "sex". Edge attributes can be put on "relation" and "generation"
 #' 	 	\item "network.info" must be a dataframe. It can be NULL. It has the following columns: sl, which is a character; alt, long and lat which are numeric; diffusion, id.diff, reproduction, mixture, cross.info, germplasm, person and year, which are factor.
-#' 	 	Possible values for diffusion are for EN: "give", "receive", "give-receive" or "".
-#' 	 	Possible values for reproduction are for EN: "sow", "harvest", "harvest-sow" or "".
-#' 	 	Possible values for selection are for EN: selection or "".
-#' 	 	Possible values for mixture are for EN: mixture or "".
+#' 	 	Possible values for diffusion are for EN: "give", "receive", "give-receive" or NA.
+#' 	 	Possible values for reproduction are for EN: "sow", "harvest", "harvest-sow" or NA.
+#' 	 	Possible values for selection are for EN: selection or NA.
+#' 	 	Possible values for mixture are for EN: mixture or NA.
 #' 	 	
 #' 	 	\item "Mdist" must be a square matrix with the same columns and rows names.
 #' 	 	}
@@ -68,6 +68,9 @@ is.get.data.output = function(
 		
 		mess = "With shinemas2R.object == \"network\", data must be a list with the following three elements: \"network\", \"network.info\", \"Mdist\"." 
 		if( !is.list(data) ) { stop(mess) }
+		if( is.null(names(data)[1]) ) { stop(mess) }
+		if( is.null(names(data)[2]) ) { stop(mess) }
+		if( is.null(names(data)[3]) ) { stop(mess) }
 		if( names(data)[1] != "network" ) { stop(mess) }
 		if( names(data)[2] != "network.info" ) { stop(mess) }
 		if( names(data)[3] != "Mdist" ) { stop(mess) }
@@ -77,7 +80,7 @@ is.get.data.output = function(
 			if( !is.network(data[[1]]) ) { stop("The first element of data must be NULL or a network object.") }
 			update.vertex.attributes = function(n = data, v.att = "year"){
 				test = get.vertex.attribute(n, v.att)
-				if( length(is.na(test)) == length(test) ) { 
+				if( length(which(is.na(test))) == length(test) ) { 
 					set.vertex.attribute(n, v.att, value = NULL)	
 					message("vertex attributes \",", v.att,",\" has been set to NULL.")
 				}			
@@ -89,8 +92,8 @@ is.get.data.output = function(
 			update.vertex.attributes(n = data[[1]], v.att = "sex")
 			
 			update.edge.attributes = function(n = data, e.att = "relation"){
-				test = get.edge.attribute(n, v.att)
-				if( length(is.na(test)) == length(test) ) { 
+				test = get.edge.attribute(n, e.att)
+				if( length(which(is.na(test))) == length(test) ) { 
 					set.edge.value(n, e.att, value = NULL)	
 					message("edge attributes \",", e.att,",\" has been set to NULL.")
 				}			
@@ -120,16 +123,16 @@ is.get.data.output = function(
 		if( !is.factor(ni$person) ) { stop("The data fame must have a column \"person\" as factor") }
 		if( !is.factor(ni$year) ) { stop("The data fame must have a column \"year\" as factor") }
 			
-		test = setdiff(ni$diffusion, c(gettext("give"), gettext("receive"), gettext("give-receive"), ""))
-		if( length(test) > 0 ) { stop("For column diffusion, the possible values are: \"", gettext("give"), "\" ,\"", gettext("receive"), "\" ,\"", gettext("give-receive"), "\", or \"\".") }
+		test = setdiff(ni$diffusion, c(gettext("give"), gettext("receive"), gettext("give-receive"), NA))
+		if( length(test) > 0 ) { stop("For column diffusion, the possible values are: \"", gettext("give"), "\" ,\"", gettext("receive"), "\" ,\"", gettext("give-receive"), "\", or NA.") }
 
-		test = setdiff(ni$reproduction, c(gettext("sow"), gettext("harvest"), gettext("harvest-sow"), ""))
-		if( length(test) > 0 ) { stop("For column reproduction, the possible values are: \"", gettext("sow"), "\" ,\"", gettext("harvest"), "\" ,\"", gettext("harvest-sow"), "\", or \"\".") }
+		test = setdiff(ni$reproduction, c(gettext("sow"), gettext("harvest"), gettext("harvest-sow"), NA))
+		if( length(test) > 0 ) { stop("For column reproduction, the possible values are: \"", gettext("sow"), "\" ,\"", gettext("harvest"), "\" ,\"", gettext("harvest-sow"), "\", or NA.") }
 
-		test = setdiff(ni$selection, c("selection", ""))
+		test = setdiff(ni$selection, c("selection", NA))
 		if( length(test) > 0 ) { stop("For column selection, the possible values are: \"selection\", or \"\".") }
 
-		test = setdiff(ni$mixture, c("mixture", ""))
+		test = setdiff(ni$mixture, c("mixture", NA))
 		if( length(test) > 0 ) { stop("For column mixture, the possible values are: \"mixture\", or \"\".") }
 		
 		}
@@ -146,8 +149,8 @@ is.get.data.output = function(
 			if( !test ) { stop(mess) }
 			}
 		
-		data = list("data" = data, "info_db" = NULL)
 		attributes(data)$shinemas2R.object = "network"
+		data = list("data" = data, "info_db" = NULL)
 		}
 
 	if( shinemas2R.object == "data-classic-seed-lots") { }
@@ -162,6 +165,7 @@ is.get.data.output = function(
 	
 	if( shinemas2R.object == "data-SR-relation") { }
 		
+	return(data)
 	}
 
 
