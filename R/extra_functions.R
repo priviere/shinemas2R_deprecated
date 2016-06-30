0e864e218635de653681305c71f09cfa240c247c
priviere <pierre@semencespaysannes.org>
2016-06-30 11:29:24 +0200

    add get.vec_variables

--- R/extra_functions.R
+++ R/extra_functions.R
@@ -11,8 +11,7 @@
 #' @author Pierre Riviere
 #' 
 
-
-
+# get.ggplot, get.table
 data.to.use = function(data, correlated_group){
 	if( is.null(correlated_group) ) {
 		data = data$data	
@@ -22,14 +21,23 @@ data.to.use = function(data, correlated_group){
 			data = data_tmp[[correlated_group]]
 		} else { stop(correlated_group, "is not a group of the data set. Possibles groups are: ", paste(names(data_tmp), collapse = ", "), ".") }
 	}
+	return(data)
 }
 
-
+# get.ggplot, get.table
 update.data.merge_g_and_s = function(data, merge_g_and_s){
 	if( merge_g_and_s ) { 
 		data$son_germplasm = sapply(as.character(data$son), function(x){unlist(strsplit(x,"_"))[1]}) 
 		data$father_germplasm = sapply(as.character(data$father), function(x){unlist(strsplit(x,"_"))[1]}) 
 	}	
+	return(data)
 }
  
-
+# get.table, format.data
+get.vec_variables = function(data, shinemas2R){
+	if( shinemas2R.object == "data-classic-relation") { vec_variables = colnames(data)[c(40: ncol(data))] }
+	if( shinemas2R.object == "data-S-relation" | shinemas2R.object == "data-SR-relation" ) { vec_variables = colnames(data)[c(44: ncol(data))] }
+	if( shinemas2R.object == "data-classic-seed-lots") { vec_variables = colnames(data)[c(15: ncol(data))] }
+	if( shinemas2R.object == "data-S-seed-lots" | shinemas2R.object == "data-SR-seed-lots" ) { vec_variables = colnames(data)[c(19: ncol(data))] }
+	return(vec_variables)
+}
