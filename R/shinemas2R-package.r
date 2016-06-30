fa174df42acd01e69c9e321af97c632fbd37c554
priviere <pierre@semencespaysannes.org>
2016-06-30 10:00:32 +0200

    add shinemas_tuto.sql

--- R/shinemas2R-package.r
+++ R/shinemas2R-package.r
@@ -5,3 +5,22 @@
 #' @description
 #' The R package \code{shinemas2R} analyses outputs from the data base SHiNeMaS (Seeds History and Network Management System). It permits desciptive analysis as well as formatting data for existing R packages to perform statistical analysis.
 NULL
+
+
+# shinemas_tuto.sql ----------
+#' SHiNeMaS in sql format use in the tutorial of the vignette
+#'
+#' @description
+#' shinemas_tuto.sql is full with data coming from the tutorial of SHiNeMaS
+#' 
+#' @usage
+#' See the vignette Appendix A Install SHiNeMaS on localhost
+#'
+#' @format
+#' sql
+#'  
+#' 
+#' @author Pierre Rivière
+"shinemas_tuto.sql"
+NULL
+
