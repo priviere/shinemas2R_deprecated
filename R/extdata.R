aa813a3720745fd7586f79e2255008f3b9c9a61e
priviere <pierre@semencespaysannes.org>
2016-07-05 09:51:23 +0200

    update Rd of shinemas_tuto.sql

---
+++ R/extdata.R
@@ -0,0 +1,15 @@
+#' SHiNeMaS in sql format use in the tutorial of the vignette
+#'
+#' @description
+#' shinemas_tuto.sql is full with data coming from the tutorial of SHiNeMaS
+#' 
+#' @usage
+#' See the vignette Appendix A Install SHiNeMaS on localhost
+#'
+#' @format
+#' sql
+#'  
+#' 
+#' @author Pierre Rivière
+NULL
+
