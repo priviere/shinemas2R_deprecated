62a4198e464dae9089c5752419ff22125492c86e
gaelleVF <gaelle.van.frank@gmail.com>
2017-08-28 15:35:13 +0200

    fix image

--- R/get.pdf.R
+++ R/get.pdf.R
@@ -652,7 +652,7 @@ get.pdf = function(
 			width = d[["width"]]
 			if(is.null(width)){ width = 1}
 			
-			cat("\\begin{figure}[!h] \n",
+			cat("\\begin{figure}[H] \n",
 				"\\begin{center} \n",
 				"\\caption{",text.to.tex(caption),"} \n",
 				"\\includegraphics[page=-,width=", width, "\\textwidth]{", we_are_here ,"/", content, "} \n",
